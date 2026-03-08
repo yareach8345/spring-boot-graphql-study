5f29ba3c49b303e740a2459898a5a3db5d03e279
yareach8345 <mine8345@naver.com>
2026-03-08 14:08:36 +0900

    project initialized
    GraphQLを勉強するためのプロジェクトを生成した。

---
+++ src/main/kotlin/com/yareach/graphqlstudy/GraphqlStudyApplication.kt
@@ -0,0 +1,11 @@
+package com.yareach.graphqlstudy
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class GraphqlStudyApplication
+
+fun main(args: Array<String>) {
+    runApplication<GraphqlStudyApplication>(*args)
+}
---
+++ src/test/kotlin/com/yareach/graphqlstudy/GraphqlStudyApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.yareach.graphqlstudy
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class GraphqlStudyApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
