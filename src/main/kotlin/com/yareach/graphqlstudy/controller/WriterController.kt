76237d8e0ec105774d34e7fef051c4fbd8491a29
yareach8345 <mine8345@naver.com>
2026-03-24 19:01:55 +0900

    feature - WriterController追加

---
+++ src/main/kotlin/com/yareach/graphqlstudy/controller/WriterController.kt
@@ -0,0 +1,48 @@
+package com.yareach.graphqlstudy.controller
+
+import com.yareach.graphqlstudy.dto.AddWriterDto
+import com.yareach.graphqlstudy.dto.PageInfoDto
+import com.yareach.graphqlstudy.dto.toPageable
+import com.yareach.graphqlstudy.model.Writer
+import com.yareach.graphqlstudy.service.WriterReadService
+import com.yareach.graphqlstudy.service.WriterWriteService
+import kotlinx.coroutines.flow.toList
+import org.springframework.graphql.data.method.annotation.Argument
+import org.springframework.graphql.data.method.annotation.MutationMapping
+import org.springframework.graphql.data.method.annotation.QueryMapping
+import org.springframework.web.bind.annotation.RestController
+
+@RestController
+class WriterController(
+    private val writerReadService: WriterReadService,
+    private val writerWriteService: WriterWriteService,
+) {
+
+    @QueryMapping
+    suspend fun getWriter(@Argument id: Long): Writer {
+        return writerReadService.getById(id)
+    }
+
+    @QueryMapping
+    suspend fun getWriters(@Argument pageRequest: PageInfoDto?): List<Writer> {
+        val pageable = pageRequest?.toPageable()
+        return writerReadService.getWriters(pageable).toList()
+    }
+
+    @QueryMapping
+    suspend fun searchWriterByName(@Argument name: String, @Argument pageRequest: PageInfoDto?): List<Writer> {
+        val pageable = pageRequest?.toPageable()
+        return writerReadService.searchWriterByName(name, pageable).toList()
+    }
+
+    @MutationMapping
+    suspend fun addWriter(@Argument input: AddWriterDto): Writer {
+        return writerWriteService.addWriter(input)
+    }
+
+    @MutationMapping
+    suspend fun deleteWriter(@Argument id: Long): Boolean {
+        writerWriteService.deleteWriter(id)
+        return true
+    }
+}
