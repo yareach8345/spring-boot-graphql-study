4b4c2ca657c10b4e1c8e510f95ae775337b70ff6
yareach <mine8345@naver.com>
2026-04-11 21:11:13 +0900

    feature - add SchemaMapping

--- src/main/kotlin/com/yareach/graphqlstudy/controller/BookController.kt
+++ src/main/kotlin/com/yareach/graphqlstudy/controller/BookController.kt
@@ -4,18 +4,22 @@ import com.yareach.graphqlstudy.dto.AddBookDto
 import com.yareach.graphqlstudy.dto.PageInfoDto
 import com.yareach.graphqlstudy.dto.toPageable
 import com.yareach.graphqlstudy.model.Book
+import com.yareach.graphqlstudy.model.Writer
 import com.yareach.graphqlstudy.service.BookReadService
 import com.yareach.graphqlstudy.service.BookWriteService
+import com.yareach.graphqlstudy.service.WriterReadService
 import kotlinx.coroutines.flow.toList
 import org.springframework.graphql.data.method.annotation.Argument
 import org.springframework.graphql.data.method.annotation.MutationMapping
 import org.springframework.graphql.data.method.annotation.QueryMapping
+import org.springframework.graphql.data.method.annotation.SchemaMapping
 import org.springframework.web.bind.annotation.RestController
 
 @RestController
 class BookController(
     private val bookReadService: BookReadService,
-    private val bookWriteService: BookWriteService
+    private val bookWriteService: BookWriteService,
+    private val writerReadService: WriterReadService,
 ) {
 
     @QueryMapping
@@ -49,5 +53,11 @@ class BookController(
     suspend fun deleteBook(@Argument id: Long): Boolean {
         bookWriteService.deleteBook(id)
         return true
+    }
+
+    @SchemaMapping
+    suspend fun writer(book: Book): Writer {
+        val writerId = book.writerId
+        return writerReadService.getById(writerId)
     }
 }
--- src/main/kotlin/com/yareach/graphqlstudy/controller/WriterController.kt
+++ src/main/kotlin/com/yareach/graphqlstudy/controller/WriterController.kt
@@ -3,19 +3,23 @@ package com.yareach.graphqlstudy.controller
 import com.yareach.graphqlstudy.dto.AddWriterDto
 import com.yareach.graphqlstudy.dto.PageInfoDto
 import com.yareach.graphqlstudy.dto.toPageable
+import com.yareach.graphqlstudy.model.Book
 import com.yareach.graphqlstudy.model.Writer
+import com.yareach.graphqlstudy.service.BookReadService
 import com.yareach.graphqlstudy.service.WriterReadService
 import com.yareach.graphqlstudy.service.WriterWriteService
 import kotlinx.coroutines.flow.toList
 import org.springframework.graphql.data.method.annotation.Argument
 import org.springframework.graphql.data.method.annotation.MutationMapping
 import org.springframework.graphql.data.method.annotation.QueryMapping
+import org.springframework.graphql.data.method.annotation.SchemaMapping
 import org.springframework.web.bind.annotation.RestController
 
 @RestController
 class WriterController(
     private val writerReadService: WriterReadService,
     private val writerWriteService: WriterWriteService,
+    private val bookReadService: BookReadService,
 ) {
 
     @QueryMapping
@@ -44,5 +48,14 @@ class WriterController(
     suspend fun deleteWriter(@Argument id: Long): Boolean {
         writerWriteService.deleteWriter(id)
         return true
+    }
+
+    @SchemaMapping
+    suspend fun books(writer: Writer): List<Book> {
+        val writerId = writer.id
+        if(writerId == null) {
+            throw RuntimeException("unknown writer id: $writerId")
+        }
+        return bookReadService.getBookByWriterId(writerId).toList()
     }
 }
